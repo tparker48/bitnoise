9fc19555eec1b6e554bed232de2ea22cf102f60a
Thomas Parker <32755663+tparker48@users.noreply.github.com>
2022-02-10 01:45:05 -0800

    create cover art script

---
+++ src/x1_cover_art.py
@@ -0,0 +1,49 @@
+import math
+import time
+
+SAMPLE_RATE = 30
+DURATION = 15
+SECONDS_PER_SAMPLE = 1.0/SAMPLE_RATE
+NUM_SAMPLES = DURATION * SAMPLE_RATE
+CONSTANT_DELTA = 2*math.pi / SAMPLE_RATE
+
+WIDTH = 60
+STAMP = '     BITNOISE     '
+EMT = ' '*(WIDTH-5)
+
+def sin(t, freq):
+    raw_sin = math.sin(t * freq * CONSTANT_DELTA)
+    adjusted = (raw_sin + 1.0) / 2.0
+    x_value = int(adjusted * WIDTH)
+    return x_value
+
+
+def paint(canvas, brush, x):
+    return canvas[:x] + brush + canvas[x + len(brush):]
+
+
+f1 = 0.15
+f2 = 0.6
+
+prefix = ' ' * 50
+for t in range(int(NUM_SAMPLES)):
+
+    s = time.time()
+    c1 = EMT
+
+    for i in range(10):
+        x1 = int(0.5*(sin(t+i, f1) + sin(t, f1*8)))
+        x2 = sin(t+i*2, f2)
+        x3 = int(0.5*(x1+x2))
+        
+        #c1 = paint(c1, '  ', x1)
+        #c1 = paint(c1, ' ', x2)
+        c1 = paint(c1, '/              \\', x3)
+    
+    #c1 = paint(c1, STAMP, int(WIDTH/8))
+
+    print(prefix + c1)
+
+    e = time.time()
+
+    time.sleep(SECONDS_PER_SAMPLE - (s-e))
